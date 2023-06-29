4f04c65fe0424d802350ab1fcad3edf6dc4fd076
marcabounader92 <marcabounader92@outlook.com>
2023-06-29 13:46:34 +0300

    1

---
+++ menu.js
@@ -0,0 +1,27 @@
+var d=document.querySelector('.burgers');
+var burgers=[
+    {name:'Whopper',price:'3$',calories:'800'},
+    {name:'Bigmac',price:'4$',calories:'1000'},
+    {name:'Double whopper',price:'5$',calories:'1200'},
+    {name:'Tripple whopper',price:'6$',calories:'1400'},
+    {name:'Tripple whopper',price:'6$',calories:'1700'},
+    {name:'Tripple whopper',price:'6$',calories:'1800'}
+    ,{name:'Tripple whopper',price:'6$',calories:'1400'}
+    ,{name:'Tripple whopper',price:'6$',calories:'1400'}
+];
+
+
+burgers.forEach((burger) =>{
+    d.innerHTML+=
+    `<div>
+        <h4>${burger.name}</h4>
+        <ul>
+            <li>price: ${burger.price}</li>
+            <li>calories: ${burger.calories}</li>
+        </ul>
+
+    </div>`;
+});
+
+
+
